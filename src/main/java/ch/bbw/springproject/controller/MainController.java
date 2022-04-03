0c977f5efe1406e2c993b926f953b770f93876eb
Röttele Leo <leo.roettele@liip.ch>
2022-04-03 13:27:38 +0200

    Used PostContruct and PreDestory to init and clear the ItemList

--- src/main/java/ch/bbw/springproject/controller/MainController.java
+++ src/main/java/ch/bbw/springproject/controller/MainController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.io.Console;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -17,19 +16,23 @@ import java.util.Optional;
 @Controller
 public class MainController {
     private final List<Item> itemList = new LinkedList<>();
-    @Autowired MyUserSession myUserSession;
 
-    public MainController(){
-        System.out.println("MainController --> Constructor is called...");
+    @PostConstruct
+    public void initItemList() {
+        System.out.println("InitItemList is called...");
         Item item1 = new Item(1, "This is the first Item", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.");
         Item item2 = new Item(2, "This is the second Item", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.");
         itemList.add(item1);
         itemList.add(item2);
     }
 
+    @PreDestroy
+    public void destroyItemList() {
+        itemList.clear();
+    }
+
     @RequestMapping("/")
     public String index(Model model) {
-        System.out.println("GetMapping index.html");
         model.addAttribute("items", itemList);
         return "index";
     }
@@ -44,17 +47,6 @@ public class MainController {
             model.addAttribute("item", null);
         }
         return "itemDetailsPage";
-    }
-
-    @PostConstruct
-    public void init() {
-        System.out.println(" MainController --> PostConstruct is called...");
-        // injected beans are ready at this point
-    }
-
-    @PreDestroy
-    public void dispose() {
-        System.out.println(" MainController --> PreDestroy is called...");
     }
 
 }
